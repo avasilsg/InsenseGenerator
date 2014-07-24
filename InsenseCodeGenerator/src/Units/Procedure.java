672f1eaaccc28d8ff0c292ccb03b444036cb2556
avasilsg <stephan.vassilev@gmail.com>
2014-07-24 02:19:40 +0100

    Fixed procedures and added some issues about nested expressions.

--- InsenseCodeGenerator/src/Units/Procedure.java
+++ InsenseCodeGenerator/src/Units/Procedure.java
@@ -1,6 +1,56 @@
 package Units;
 
+import java.beans.Expression;
+import java.util.LinkedList;
+
+import Units.BasicUnits.Field;
+
 public class Procedure
 {
+    private String name;
+    private String type;
+    private LinkedList<Field> parameters;
+    private LinkedList<Expression> expressions;
     
+    public Procedure()
+    {
+        name = null;
+        type = null;
+        parameters = new LinkedList<>();
+        expressions = new LinkedList<>();
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public LinkedList<Field> getParameters()
+    {
+        return parameters;
+    }
+    public void setParameters(LinkedList<Field> parameters)
+    {
+        this.parameters = parameters;
+    }
+    public String getType()
+    {
+        return type;
+    }
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+    public LinkedList<Expression> getExpressions()
+    {
+        return expressions;
+    }
+    public void setExpressions(LinkedList<Expression> expressions)
+    {
+        this.expressions = expressions;
+    }
+     
 }
