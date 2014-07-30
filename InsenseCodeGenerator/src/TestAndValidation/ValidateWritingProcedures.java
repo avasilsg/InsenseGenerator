2f9ae13d8fbf8f5e4f67adf7bd6da14682baf9ab
avasilsg <stephan.vassilev@gmail.com>
2014-07-30 03:47:55 +0100

    test and validation provided.

---
+++ InsenseCodeGenerator/src/TestAndValidation/ValidateWritingProcedures.java
@@ -0,0 +1,16 @@
+package TestAndValidation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ValidateWritingProcedures
+{
+    
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+    
+}
---
+++ InsenseCodeGenerator/src/TestAndValidation/ValidateXMLParsing.java
@@ -0,0 +1,16 @@
+package TestAndValidation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ValidateXMLParsing
+{
+    
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+    
+}
